b655d020e56ca4c660f960c8586dec9c2792abe6
Leon Lan <l.lan@vu.nl>
2024-05-20 12:17:48 +0200

    Return solution cost

--- lkh/__init__.py
+++ lkh/__init__.py
@@ -11,50 +11,62 @@ class NoToursException(Exception):
     pass
 
 
-def solve(solver='LKH', problem=None, **params):
-    assert shutil.which(solver) is not None, f'{solver} not found.'
-    assert ('problem_file' in params) ^ (problem is not None), 'Specify a problem object *or* a path.'
+def solve(solver="LKH", problem=None, **params):
+    assert shutil.which(solver) is not None, f"{solver} not found."
+    assert ("problem_file" in params) ^ (
+        problem is not None
+    ), "Specify a problem object *or* a path."
 
-    if 'problem_file' in params:
+    if "problem_file" in params:
         # annoying, but necessary to get the original problem dimension
-        problem = LKHProblem.load(params['problem_file'])
+        problem = LKHProblem.load(params["problem_file"])
 
     if not isinstance(problem, LKHProblem):
-        warnings.warn('Subclassing LKHProblem is recommended. Proceed at your own risk!')
+        warnings.warn(
+            "Subclassing LKHProblem is recommended. Proceed at your own risk!"
+        )
 
     if len(problem.depots) > 1:
-        warnings.warn('LKH-3 cannot solve multi-depot problems.')
+        warnings.warn("LKH-3 cannot solve multi-depot problems.")
 
-    prob_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    prob_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
     problem.write(prob_file)
-    prob_file.write('\n')
+    prob_file.write("\n")
     prob_file.close()
-    params['problem_file'] = prob_file.name
+    params["problem_file"] = prob_file.name
 
-    if 'tour_file' not in params:
-        tour_file = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        params['tour_file'] = tour_file.name
+    if "tour_file" not in params:
+        tour_file = tempfile.NamedTemporaryFile(mode="w", delete=False)
+        params["tour_file"] = tour_file.name
         tour_file.close()
 
-    par_file = tempfile.NamedTemporaryFile(mode='w+', delete=False)
-    par_file.write('SPECIAL\n')
+    par_file = tempfile.NamedTemporaryFile(mode="w+", delete=False)
+    par_file.write("SPECIAL\n")
     for k, v in params.items():
-        par_file.write(f'{k.upper()} = {v}\n')
+        par_file.write(f"{k.upper()} = {v}\n")
     par_file.close()
 
     try:
         # stdin=DEVNULL for preventing a "Press any key" pause at the end of execution
-        subprocess.check_output([solver, par_file.name], stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL)
+        subprocess.check_output(
+            [solver, par_file.name], stderr=subprocess.STDOUT, stdin=subprocess.DEVNULL
+        )
     except subprocess.CalledProcessError as e:
         raise Exception(e.output.decode())
 
-    if not os.path.isfile(params['tour_file']) or os.stat(params['tour_file']).st_size == 0:
-        raise NoToursException(f"{params['tour_file']} does not appear to contain any tours. LKH probably did not find solution.")
+    if (
+        not os.path.isfile(params["tour_file"])
+        or os.stat(params["tour_file"]).st_size == 0
+    ):
+        raise NoToursException(
+            f"{params['tour_file']} does not appear to contain any tours. LKH probably did not find solution."
+        )
 
     # the tour file produced by LKH-3 includes dummy nodes to indicate depots
     # for example, if a problem has DIMENSION=32 (1 depot node + 31 task nodes),
     # the tour file will have a SINGLE tour with DIMENSION=36 (5 depot nodes + 31 task nodes)
-    solution = LKHProblem.load(params['tour_file'])
+    solution = LKHProblem.load(params["tour_file"])
+
     tour = solution.tours[0]
     # convert this tour to multiple routes
     routes = []
@@ -68,10 +80,22 @@ def solve(solver='LKH', problem=None, **params):
             route.append(node)
     routes.append(route)
 
+    # Extract the cost from the tour file.
+    with open(params["tour_file"], "r") as fh:
+        solution_data = fh.read()
+
+    pattern = "Length = "
+    start = solution_data.find(pattern)
+    end = start + 1
+    while end < len(solution_data) and solution_data[end] != "\n":
+        end += 1
+
+    cost = int(solution_data[start + len(pattern) : end])
+
     os.remove(par_file.name)
-    if 'prob_file' in locals():
+    if "prob_file" in locals():
         os.remove(prob_file.name)
-    if 'tour_file' in locals():
+    if "tour_file" in locals():
         os.remove(tour_file.name)
 
-    return routes
+    return routes, cost
